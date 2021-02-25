a844cbe216cc0c6166354a0e94430758be26fc01
superSyd <37779021+superSyd@users.noreply.github.com>
2021-02-25 14:47:28 +1100

    Update server.js

--- server.js
+++ server.js
@@ -1,105 +1,83 @@
+// server.js
+// where your node app starts
+
+// init project
+var moment = require('moment');
+var momentTimezone = require('moment-timezone');
 var express = require('express');
-var bodyParser = require('body-parser');
 var app = express();
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
-app.use(middleware = (req,res,next) =>{
-console.log(req.method + " "+req.path+" - "+req.ip)
-next();
-})
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
 
-app.get('/now', (req,res,next) => {
-  
-  req.time = new Date().toString();
-  next();
-
-}, (req,res) => {
-  res.send({time: req.time})
-})
-
-app.get("/:word/echo", (req,res) => {
-  const {word} = req.params
-  res.send({
-    echo: word
-  })
-})
-
-app.get("/name", (req,res)=>{
-  const firstname = req.query.first;
-  const lastname = req.query.last;
-  res.send({
-    name : firstname + " "+lastname
-  })
-})
-
-//console.log("Hello World");
-/*
-app.get("/", (req, res) => {
-  res.send("Hello Express");
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
 });
-*/
 
-app.get("/", (req,res) => {
-  res.sendFile(__dirname + "/views/index.html")
+
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
+
+app.get("/api/timestamp", (req,res)=>{
+
+    myUnixDate = moment().unix()*1000;
+    myUtcDate = moment().format("ddd, D MMM YYYY HH:mm:ss");
+    timeZone = momentTimezone.tz(momentTimezone.tz.guess()).format('z');
+
+    res.json({unix: myUnixDate, utc: myUtcDate +" "+timeZone})
 })
 
-app.use("/public",express.static(__dirname + "/public"))
 
-let myObject = {"message":"Hello json"}
-let myObjectUpperCase = {"message":"HELLO JSON"}
+app.get("/api/timestamp/:date_string", (req,res)=>{
+  const date = req.params.date_string;
 
-app.get("/json", (req, res) => {
 
-  if(process.env.MESSAGE_STYLE === "uppercase"){
-    
-    res.json(myObjectUpperCase);
-  } else {
-    res.json(myObject);
+  var myDate;
+  var myUnixDate;
+  var myUtcDate;
+  var timeZone;
+
+
+    if(isNaN(date)){
+
+    myDate = new Date(date);
+  
+    myUnixDate = moment(myDate.toString()).unix() * 1000;
+    myUtcDate = moment(myDate.toString()).format("ddd, D MMM YYYY HH:mm:ss");
+  timeZone = momentTimezone.tz(momentTimezone.tz.guess()).format('z');
+
+  } else if(!isNaN(date)){
+
+  myDate = new Date(parseInt(date));
+  
+  myUnixDate = moment(myDate.toString()).unix() * 1000;
+  myUtcDate = moment(myDate.toString()).format("ddd, D MMM YYYY HH:mm:ss");
+  timeZone = momentTimezone.tz(momentTimezone.tz.guess()).format('z');
+
   }
-  
-});
 
-app.post("/name", (req,res)=>{
-  const firstname = req.body.first;
-  const lastname = req.body.last;
+  if(isNaN(myUnixDate) && isNaN(myUtcDate)){
+    res.json({error: "Invalid Date"})
+  } else {
+    res.json({unix: myUnixDate, utc: myUtcDate +" "+timeZone})
 
-  res.send({
-    name: firstname +" "+ lastname
-  })
+  }
+
+  //console.log(myUnixDate, myUtcDate)
+
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
